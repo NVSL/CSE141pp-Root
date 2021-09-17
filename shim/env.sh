3b8dbd67df6153e646679d4aa0fb69d908b65d60
root <root@swanson-dev-192.168.4.21>
2021-09-16 20:49:13 -0700

    terrible hack for cpupower

--- shim/env.sh
+++ shim/env.sh
@@ -1,5 +1,5 @@
 
-if [ "3.10.0-1160.25.1.el7.x86_64" = `uname -r` ]
+if [ "3.10.0-1160.25.1.el7.x86_64" = `uname -r` ]; then
 
    export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
    export PATH=$PWD/bin:$PATH
